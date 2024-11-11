2158f517067eae79f503c136d121b89f6e2cdc22
Rom_Yura <romashuk98@gmail.com>
2024-11-11 20:48:44 +0100

    Initial commit

---
+++ src/com/gmailromashchuk/Store.java
@@ -0,0 +1,38 @@
+package com.gmailromashchuk;
+
+public class Store {
+    public static void main(String[] args) {
+        String name;
+        String product;
+        float price;
+        String address;
+
+        name = "Alice.";
+        product = "Smartphone,";
+        price = 349.99f;
+        address = "Moon Street, 10.";
+
+        System.out.println("Order No 1 Client: " + name);
+        System.out.println("Product: " + product);
+        System.out.println("Price EUR " + price + ".");
+        System.out.println("Address: " + addr
+        System.out.println("===================================");
+
+        String name1;
+        String product1;
+        float price1;
+        String address1;
+
+        name1 = "Tom.";
+        product1 = "laptop,";
+        price1 = 570.95f;
+        address1 = "Terra Street, 17.";
+
+
+        System.out.println("Order No 2 Client: " + name1);
+        System.out.println("Product: " + product1);
+        System.out.println("Price EUR " + price1 + ".");
+        System.out.println("Address: " + address1);
+
+    }
+}
