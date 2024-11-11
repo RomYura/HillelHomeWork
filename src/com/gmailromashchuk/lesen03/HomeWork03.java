a60b12efdb3ff4eaa7e492854aa0053be78c832c
Rom_Yura <romashuk98@gmail.com>
2024-11-11 22:05:37 +0100

    Lesson03 (Разобрался +++)

--- src/com/gmailromashchuk/lesen03/HomeWork03.java
+++ src/com/gmailromashchuk/lesen03/HomeWork03.java
@@ -3,5 +3,8 @@ package com.gmailromashchuk.lesen03;
 public class HomeWork03 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int res;
+
     }
 }
